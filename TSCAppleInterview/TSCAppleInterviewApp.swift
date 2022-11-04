6ffbf29208612cbd7bf1cfaa3c1c4b00c284adc9
Carlos Andrés Sánchez <carlosandres.sh@gmail.com>
2022-11-03 22:18:47 -0700

    Initial Commit

---
+++ TSCAppleInterview/TSCAppleInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TSCAppleInterviewApp.swift
+//  TSCAppleInterview
+//
+//  Created by Carlos Sánchez on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct TSCAppleInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
